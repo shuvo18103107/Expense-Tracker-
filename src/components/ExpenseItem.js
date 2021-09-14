0e9e66fbc5493b3354fe1d59890a7224183a7992
shuvo18103107 <18103107@iubat.edu>
2021-09-14 22:20:10 +0600

    ✅ concept of composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
+import Card from './Card';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
-function ExpenseItem() {
+function ExpenseItem(props) {
+
     return (
-        <div className='expense-item'>
-            <div> date </div>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>title</h2>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <div className='expense-item__price'>amount</div>
-        </div>
+        </Card>
     );
 }
 export default ExpenseItem;
