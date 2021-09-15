45f13c27c48458ecfc12bde11414ad4efba871e2
shuvo18103107 <18103107@iubat.edu>
2021-09-15 17:24:48 +0600

    ✅ Adding Form layout

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
+
+import '../NewExpense/NewExpense.css'
+const NewExpense = () => {
+    return <div className='new-expense'>
+        <ExpenseForm />
+    </div>
+
+}
+export default NewExpense;
